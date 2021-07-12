4c641068b589cb6c191c4700149583a4473a6c53
ctamayo74 <c.tamayo1974@gmail.com>
2021-07-11 21:02:32 -0600

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/compartidos/navbar/navbar.component';
+import { FooterComponent } from './componentes/compartidos/footer/footer.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { BodyComponent } from './componentes/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
