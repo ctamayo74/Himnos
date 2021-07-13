2a199a108fac2a4159c80adb4e32f149b2f7db55
ctamayo74 <c.tamayo1974@gmail.com>
2021-07-13 06:31:17 -0600

    Cambios funcionamiento routing del navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './componentes/body/body.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'inicio', component: InicioComponent },
+  { path:'body', component: BodyComponent },
+  { path: '**', pathMatch:'full', redirectTo:'inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
